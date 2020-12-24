0eb30d649a4aea25eed4cd8bc7881f06ffd14e10
GalinRaph <76114806+GalinRaph@users.noreply.github.com>
2020-12-25 02:10:53 +0500

    Delete Task_4.py

--- Task_4.py
+++
@@ -1,8 +0,0 @@
-usernumber = int(input('Поиск максимальной цифры в вашем числе:'))
-array = []
-
-while usernumber > 10:
-    array.append(usernumber % 10)
-    usernumber = usernumber // 10
-maxarray = max(array)
-print(maxarray)
