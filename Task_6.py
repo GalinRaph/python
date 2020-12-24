60111e7241aa9a6e2dd2fcd9eea7d77c92325f36
GalinRaph <76114806+GalinRaph@users.noreply.github.com>
2020-12-25 01:36:08 +0500

    Delete Task_6.py

--- Task_6.py
+++
@@ -1,7 +0,0 @@
-firstresult = int(input('Введите расстояние, которое вы пробежали в первый день (в км):'))
-desiredresult = int(input('Введите желаемый результат при ежедневном десятипроцентном повышении расстояния (в км):'))
-dayresult = 0
-while desiredresult > firstresult:
-    firstresult = firstresult * 1.1
-    dayresult = dayresult + 1
-print('На ' + str(dayresult) + '-й день вы пробежите не менее ' + str(desiredresult) + ' км')
