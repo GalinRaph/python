9ff94d40edea9c3961c9e2f330c1af3d7ee5b7f1
GalinRaph <76114806+GalinRaph@users.noreply.github.com>
2020-12-25 01:35:33 +0500

    Delete Task_3.py

--- Task_3.py
+++
@@ -1,3 +0,0 @@
-usernumber = int(input('Введите число n для вычисления функции вида "n+nn+nnn":'))
-functionsum = usernumber + int(str(usernumber)+str(usernumber)) + int(str(usernumber)+str(usernumber)+str(usernumber))
-print(str(functionsum))
